2276c341a5b0e99948f19aa7eee8ec202a5a78fa
ly <liuyue19891018@163.com>
2015-02-14 16:42:46 +0800

    test

--- src/CmdLineOptParser.h
+++ src/CmdLineOptParser.h
@@ -17,7 +17,7 @@
  GNU General Public License for more details.
  
  You should have received a copy of the GNU General Public License
- along with QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
+ along wit QGROUNDCONTROL. If not, see <http://www.gnu.org/licenses/>.
  
  ======================================================================*/
 
